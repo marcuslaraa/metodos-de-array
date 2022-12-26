ed81de0edf5b3606c678fd7f9562012d2777a09c
MarcusLara <101606567+marcuslaraa@users.noreply.github.com>
2022-12-26 15:27:19 -0300

    Metodo-map

---
+++ app/main.js
@@ -0,0 +1,32 @@
+let livros = [];
+const endPointdaAPI =
+  'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+
+getBuscarLivrosDaAPI();
+const elementoParaInserirLivros = document.getElementById('livros');
+async function getBuscarLivrosDaAPI() {
+  const res = await fetch(endPointdaAPI);
+  livros = await res.json();
+  let livrosComDesconto = aplicarDesconto(livros);
+  exibirOslivrosNatela(livrosComDesconto);
+}
+
+function exibirOslivrosNatela(listaDeLivros) {
+  listaDeLivros.forEach(livro => {
+    elementoParaInserirLivros.innerHTML += `
+    <div class="livro">
+      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+      <h2 class="livro__titulo">
+        ${livro.titulo}
+      </h2>
+      <p class="livro__descricao">${livro.autor}</p>
+      <p class = "livro_quantidade">Quantidade disponivel: ${
+        livro.quantidade
+      }</p>
+      <p class="livro__preco" id="preco"> R$${livro.preco.toFixed(2)}</p>
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+      </div>
+    </div>`;
+  });
+}
